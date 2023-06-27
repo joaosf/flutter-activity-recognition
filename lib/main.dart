b4328ed85fdbaee935fac8ca7745fef31c6e82ef
joaosf <t.joao.ferreira@zaffari.com.br>
2023-06-27 17:18:44 -0300

    New features: - get geo data - post info to api

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'dart:async';
 import 'dart:async';
+import 'dart:async';
+import 'dart:async';
+import 'dart:convert';
 import 'dart:developer' as dev;
+import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_activity_recognition/flutter_activity_recognition.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(ExampleApp());
 
@@ -16,13 +24,103 @@ class _ExampleAppState extends State<ExampleApp> {
   final _activityStreamController = StreamController<Activity>();
   StreamSubscription<Activity>? _activityStreamSubscription;
 
-  void _onActivityReceive(Activity activity) {
+  static FutureOr<String?> getDeviceIdentifier() async {
+    String? deviceName;
+    String? deviceVersion;
+    String? identifier = "";
+    final DeviceInfoPlugin deviceInfoPlugin = new DeviceInfoPlugin();
+    try {
+      if (Platform.isAndroid) {
+        var build = await deviceInfoPlugin.androidInfo;
+        deviceName = build.model;
+        deviceVersion = build.version.toString();
+        identifier = build.androidId;  //UUID for Android
+      } else if (Platform.isIOS) {
+        var data = await deviceInfoPlugin.iosInfo;
+        deviceName = data.name;
+        deviceVersion = data.systemVersion;
+        identifier = data.identifierForVendor;  //UUID for iOS
+      }
+    } on PlatformException {
+      print('Failed to get platform version');
+    }
+
+//if (!mounted) return;
+//     return [deviceName, deviceVersion, identifier];
+    return identifier;
+  }
+
+  postData(Map data) async {
+    try {
+      String? deviceId = await getDeviceIdentifier();
+      var response = await http.post(
+          Uri.parse("https://us-central1-mock-api-cc43f.cloudfunctions.net/mock/"+deviceId!),
+          headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+          },
+          body: jsonEncode(data)
+      );
+      print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  FutureOr<void> _onActivityReceive(Activity activity) async {
     dev.log('Activity Detected >> ${activity.toJson()}');
     _activityStreamController.sink.add(activity);
+
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    String long = position.longitude.toString();
+    String lat = position.latitude.toString();
+    String alt = position.altitude.toString();
+
+    var dataToPost = {
+      "geo": {
+        "lat": lat,
+        "long": long,
+        "alt": alt
+      },
+      "activity": {
+        "type": activity.type.toString(),
+        "confidence": activity.confidence.toString(),
+      },
+      "date": DateTime.now().toString()
+    };
+
+    print(dataToPost);
+    await postData(dataToPost);
   }
 
   void _handleError(dynamic error) {
     dev.log('Catch Error >> $error');
+  }
+
+  FutureOr<void> checkGPSPermission() async {
+    bool servicestatus = await Geolocator.isLocationServiceEnabled();
+
+    if(servicestatus){
+      print("GPS service is enabled");
+    }else{
+      print("GPS service is disabled.");
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Location permissions are denied');
+      }else if(permission == LocationPermission.deniedForever){
+        print("'Location permissions are permanently denied");
+      }else{
+        print("GPS Location service is granted");
+      }
+    }else{
+      print("GPS Location permission granted.");
+    }
   }
 
   @override
@@ -49,6 +147,8 @@ class _ExampleAppState extends State<ExampleApp> {
       _activityStreamSubscription = activityRecognition.activityStream
           .handleError(_handleError)
           .listen(_onActivityReceive);
+
+      await checkGPSPermission();
     });
   }
 
